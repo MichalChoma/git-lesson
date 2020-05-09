f154e717d2bdb82ef16c20d861670415bbe7a47d
Michal Choma <chomamichal01@gmail.com>
2020-05-10 00:27:31 +0200

    Add initial files

---
+++ js/skrypt.js
@@ -0,0 +1,10 @@
+/* const myArray = [];
+const title = document.querySelector('#menu');
+var login = document.getElementById('login');
+
+
+myArray.push(login);
+
+console.log(myArray);
+*/
+console.log("hello world");
